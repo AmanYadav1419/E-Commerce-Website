8d7700ee9e5871a75772bda4393c68c5668b613f
[abhishek palve] <abhishekpalve55@gmail.com>
2024-01-10 14:59:18 +0530

    changes & updates done in shop.jsx

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../components/PageHeader'
 
+const showResults = "Showing 01 - 12 of 139 Results"
+import Data from "../products.json"
+
 const Shop = () => {
+   const [GridList,setGridList] = useState(true);
+   const [products,setproducts] = useState(Data);
+   console.log(products)
   return (
     <div>
       <PageHeader title="Our Shop Page" curPage="Shop"/>
+      {/*shop page */}
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                {/*layout and title here */}
+                <div className="shop-title d-flex flex-warp justify-content-between">
+                  <p>{showResults}</p>
+                  <div className={`product-view-mode ${GridList ? "gridActive":"listActive"}`}>
+                    <a className='grid' onClick={() => setGridList(!GridList)}>
+                      <i className='icofont-ghost'></i>
+                    </a>
+                    <a className='list' onClick={() => setGridList(!GridList)}>
+                      <i className='icofont-listine-dots'></i>
+                    </a>
+                  </div>
+                </div>
+                <div>
+                  
+                </div>
+
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">
+                right side
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
